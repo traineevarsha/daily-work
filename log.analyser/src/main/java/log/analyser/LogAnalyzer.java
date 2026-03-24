6c366204fb86bfa70439ff4c4bccdcb124b2afbe
traineevarsha <trainee.varsha@gmail.com>
2026-03-24 08:46:36 +0530

    final identity fix

--- log.analyser/src/main/java/log/analyser/LogAnalyzer.java
+++ log.analyser/src/main/java/log/analyser/LogAnalyzer.java
@@ -1,5 +1,35 @@
 package log.analyser;
 
-public class LogAnalyzer {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+public class LogAnalyzer {
+	public static void main(String[] args) {
+
+		Map<String, Integer> map = new HashMap<>();
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("system.log"));
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				String[] logs = line.split(",");
+				String logLevel = logs[0];
+
+				if (map.containsKey(logLevel)) {
+					map.put(logLevel, map.get(logLevel) + 1);
+				} else {
+					map.put(logLevel, 1);
+				}
+				reader.close();
+			}
+		} catch (IOException e) {
+			System.out.println("File not found");
+
+		}
+
+	}
 }
