a1958af1d9fcf8a944b8933060e0a574d2a43988
traineevarsha <trainee.varsha@gmail.com>
2026-03-26 12:20:25 +0530

    Day12_Basic_ToDoList

---
+++ Day12_Simple_ToDoList/ToDoList.js
@@ -0,0 +1,44 @@
+
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+function addTodo() {
+    const input = document.getElementById("todo-input");
+    const text = input.value.trim();
+    if (text === '') return;
+    todos.push({
+        text: text,
+        completed: false
+    });
+    input.value = "";
+    saveTodos();
+    renderTodos();
+}
+function deleteTodo(index) {
+    todos.splice(index, 1);
+    saveTodos();
+    renderTodos();
+}
+function toggleComplete(index) {
+    todos[index].completed = !todos[index].completed;
+    saveTodos();
+    renderTodos();
+}
+function renderTodos() {
+    const list = document.getElementById("todo-list");
+    list.innerHTML = "";
+    todos.forEach((todo, index) => {
+        const li = document.createElement("li");
+        if (todo.completed) li.classList.add('completed');
+        li.innerHTML = `
+            <span>${todo.text}</span>
+            <div>
+                <button class ="tick-mark" onclick="toggleComplete(${index})">✔</button>
+                <button class = "cross-mark" onclick="deleteTodo(${index})">X</button>
+            </div>
+        `;
+        list.appendChild(li);
+    });
+}
+renderTodos();
